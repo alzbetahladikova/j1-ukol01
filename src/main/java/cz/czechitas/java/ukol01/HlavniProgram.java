56a817ee7536f649a29a6746cab0d6c44fc6316e
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-14 20:05:34 +0100

    Prasátko OK,

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -3,12 +3,51 @@ package cz.czechitas.java.ukol01;
 import cz.czechitas.java.ukol01.engine.*;
 
 public class HlavniProgram {
-
+    Turtle zofka = new Turtle();
     public void start() {
-        Turtle zofka = new Turtle();
+
 
         //TODO implementace domácího úkolu
+
+
+        nakresliPrasatko(200,200);
     }
+
+    private void nakresliPrasatko(double x,double y) {
+        zofka.setLocation(x, y);
+        zofka.turnLeft(90);
+
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(70.7);
+        zofka.turnLeft(90);
+        zofka.move(70.7);
+        zofka.turnLeft(45);
+        zofka.move(20);
+        double predniNohyX = zofka.getX();
+        double predniNohyY = zofka.getY();
+        zofka.turnRight(120);
+        zofka.move(40);
+        zofka.setLocation(predniNohyX, predniNohyY);
+        zofka.turnLeft(60);
+        zofka.move(40);
+        zofka.setLocation(predniNohyX, predniNohyY);
+        zofka.turnLeft(60);
+        zofka.move(70);
+        double zadniNohyX = zofka.getX();
+        double zadniNohyY = zofka.getY();
+        zofka.turnRight(120);
+        zofka.move(40);
+        zofka.setLocation(zadniNohyX, zadniNohyY);
+        zofka.turnLeft(60);
+        zofka.move(40);
+        zofka.turnLeft(150);
+
+}
 
     public static void main(String[] args) {
         new HlavniProgram().start();
